62ff73a10dbc71240bb0855555deb4271cbeb536
Juan Gonzalez <jugonzal@gmail.com>
2018-08-28 10:25:37 -0400

    added a few functions

--- multiply.js
+++ multiply.js
@@ -1,7 +1,11 @@
 // - input 2 numbers between 6 and 10
 
-var left = process.argv.slice(2)[0]
-var right = process.argv.slice(2)[1]
+function oneParam(which) {
+  return process.argv.slice(2)[which]
+}
+
+var left = oneParam(0)
+var right = oneParam(1)
 
 // console.log(left)
 // console.log(right)
@@ -13,27 +17,36 @@ var right = process.argv.slice(2)[1]
 //   - ringer = 9
 //   - pinky = 10
 
-var leftGrouped = left - 5
+
+function  calcGrouped(n) {
+  return n - 5
+}
+
+var leftGrouped = calcGrouped(left)
 
 //  - do the same with my right hand for the second number
 
-var rightGrouped = right - 5
+var rightGrouped = calcGrouped(right)
 
-console.log("left grouped", leftGrouped)
-console.log("right grouped", rightGrouped)
+// console.log("left grouped", leftGrouped)
+// console.log("right grouped", rightGrouped)
 
 //  - join those 2 groups of fingers
 //  - count them and multiply by 10   <--- RESULT 1
 
-var grouped = (leftGrouped + rightGrouped) * 10
+var grouped = (calcGrouped(left) + calcGrouped(right)) * 10
 
 //  - count my left fingers left floating
 
-var leftFloating = 5 - leftGrouped
-var rightFloating = 5 - rightGrouped
+function calcFloating(n) {
+  return 5 - n
+}
 
-console.log("left floating", leftFloating)
-console.log("right floating", rightFloating)
+var leftFloating = calcFloating(leftGrouped)
+var rightFloating = calcFloating(rightGrouped)
+
+// console.log("left floating", leftFloating)
+// console.log("right floating", rightFloating)
 
 //  - and multiply them by the fingers left floating in my right hand
 
